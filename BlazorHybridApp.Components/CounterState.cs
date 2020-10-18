caa8b81482750738e8f7e384c95cf121438e6398
Daniel Roth <daroth@microsoft.com>
2020-10-18 00:21:39 -0700

    Add Blazor hybrid app

---
+++ BlazorHybridApp.Components/CounterState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BlazorHybridApp.Components
+{
+    public class CounterState
+    {
+        public int CurrentCount { get; private set; }
+
+        public void IncrementCount()
+        {
+            CurrentCount++;
+            StateChanged?.Invoke();
+        }
+
+        public event Action StateChanged;
+    }
+}
---
+++ BlazorHybridApp/App.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net.Http;
+using BlazorHybridApp.Components;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.MobileBlazorBindings;
+using Microsoft.MobileBlazorBindings.WebView;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace BlazorHybridApp
+{
+    public class App : Application
+    {
+        public App()
+        {
+            BlazorHybridHost.AddResourceAssembly(GetType().Assembly, contentRoot: "WebUI/wwwroot");
+
+            var host = MobileBlazorBindingsHost.CreateDefaultBuilder()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    // Adds web-specific services such as NavigationManager
+                    services.AddBlazorHybrid();
+
+                    // Register app-specific services
+                    services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://10.0.2.2:9816") });
+                    services.AddSingleton<CounterState>();
+                })
+                .Build();
+
+            MainPage = new ContentPage { Title = "My Application" };
+            host.AddComponent<Main>(parent: MainPage);
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
